eff1601f7c8f588fe37a3891b3c7b1d8bc679ee7
yeonng29 <yeonng29@naver.com>
2022-12-05 14:24:54 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
-  #  path('<int:pk>/', views.single_post_page),
-  #  path('', views.index),
 
   
 ]
